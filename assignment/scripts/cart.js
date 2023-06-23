b4506c6af486b2f1bd92f95549aec56c26b87070
Lucas Martin <driftpvp@gmail.com>
2023-06-22 20:42:47 -0500

    tough

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,6 +1,54 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = []
+const maxItems = 5;
+/**
+ * 
+ * @param {string} item item to add to array
+ */
+function addItem(item){
+basket.push(item)
+return true
+function isFull(){
+    if (addItem < maxItems){
+    return true
+    }
+    if (addItem === maxItems){
+    return false
+    }
+}
+}
+addItem("pears");
+addItem("apples");
+addItem("bananas");
+console.log(`'Basket is ${basket}"`);
+
+function listItems(){
+    for (element of basket){
+        console.log(element);
+    }
+    return basket
+}
+console.log(listItems(basket));
+
+function empty(){
+basket = []
+return basket
+}
+console.log(empty(basket));
+console.log(`Basket is now ${basket}"`);
+
+function full(){
+    while (basket.length < maxItems){
+    return false}
+    if (basket.length === maxItems){
+    return true
+    }  
+}
+
+
+
 
 
 
